5b6ed0b1cb5f402d3e2c9554050bc119f65eb13f
ls195 <lucastascheit195@gmail.com>
2025-04-19 11:15:52 +0200

    Service_A implementiert

---
+++ flask_api_5.py
@@ -0,0 +1,128 @@
+from flask import Flask, request, jsonify
+from typing import List
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import PrimaryKeyConstraint
+from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from flask_caching import Cache
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from datetime import datetime, date
+
+app = Flask(__name__)
+api = Api(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres_user:postgres_pw@192.168.178.52:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['JWT_SECRET_KEY'] ='super_duper_secret'
+
+app.config['CACHE_TYPE'] = 'SimpleCache'
+app.config['CACHE_DEFAULT_TIMEOUT']=300     #globales Caching --> Aktualisierung des Cache alle 300 Sekunden
+
+cache = Cache(app)
+
+jwt = JWTManager(app)
+
+@app.route("/login", methods=['POST'])              ##JWT Initialisierung über /login
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if username != 'test' or password != 'test':
+        return jsonify({'msg':'Bad username or password'}), 401
+    access_token = create_access_token(identity = username)
+    return jsonify(access_token=access_token) 
+
+#Hier wird die Datenbank anhand der Model-Klassen definiert
+db = SQLAlchemy(app)
+
+#Model definieren
+class Kunde(db.Model):
+    __tablename__ = 'kunde'
+    kd_nr:Mapped[int]=mapped_column(primary_key = True)
+    vorname:Mapped[str]
+    nachname:Mapped[str]
+    strasse:Mapped[str]
+    plz:Mapped[int]
+    ort:Mapped[str]
+    vorwahl:Mapped[str]
+    telefon:Mapped[str]
+    geburtsdatum:Mapped[date]
+    ledig:Mapped[bool]
+    rabatt:Mapped[float]
+    letzter_zugriff:Mapped[datetime]
+    k_a:Mapped[List["Auftrag"]]=relationship()
+
+class Auftrag(db.Model):
+    __tablename__ = 'auftrag'
+    auft_nr:Mapped[int]=mapped_column(primary_key = True)
+    bestelldat:Mapped[date]
+    lieferdat:Mapped[date]
+    zahlungsziel:Mapped[date]
+    zahlungseingang:Mapped[date]
+    mahnung:Mapped[int]
+    fk_k_a:Mapped[int]=mapped_column(ForeignKey("kunde.kd_nr"))                   #Foreign key auf kunde.kd_nr  
+    fk_s_a:Mapped[int]=mapped_column(ForeignKey("shop.shop_nr"))                   #Foreign key auf shop.shop_nr
+    
+
+
+class bestellposition(db.Model):
+    __tableame__='bestellposition'
+    fk_auftrag:Mapped[int]=mapped_column(ForeignKey("auftrag.auft_nr"))
+    position:Mapped[int]
+    fk_artikel:Mapped[int]=mapped_column(ForeignKey("artikel.art_nr"))
+    anzahl:Mapped[int]
+
+    __table_args__=(
+            PrimaryKeyConstraint('fk_auftrag', 'fk_artikel'),
+            )
+
+class Artikel(db.Model):
+    __tablename__='artikel'
+    art_nr:Mapped[int]=mapped_column(primary_key=True)
+    artikelbezeichnung:Mapped[str]
+    einzelpreis:Mapped[float]
+    gewicht:Mapped[float]
+    fk_hersteller:Mapped[float]=mapped_column(ForeignKey("hersteller.herst_nr"))
+    
+
+class Hersteller(db.Model):
+    __tablename__='hersteller'
+    herst_nr:Mapped[int]=mapped_column(primary_key=True)
+    herstellerbezeichnung:Mapped[str]
+
+class Stadt(db.Model):
+    __tablename__='stadt'
+    stadt_nr:Mapped[int]=mapped_column(primary_key=True)
+    stadt:Mapped[str]
+    lat:Mapped[float]
+    lot:Mapped[float]
+
+class Shop(db.Model):
+    __tablename__='shop'
+    shop_nr:Mapped[int]=mapped_column(primary_key=True)
+    fk_shoptyp:Mapped[int]
+    strasse:Mapped[str]
+    plz:Mapped[str]
+    fk_stadt:Mapped[int]=mapped_column(ForeignKey("stadt.stadt_nr"))
+    s_a:Mapped[List["Auftrag"]]=relationship()                                         
+
+
+
+class kunde_list(Resource):
+    @cache.cached(timeout=60)      #routenspezifisches Caching --> Erneuerung des Cache alle 60 Sekunden
+    @jwt_required()
+    def get(self):                              #Service Initialisierung   
+        now = datetime.utcnow().isoformat()
+        print(f"Uhrzeit der Generierung des Cache: {now}")
+        current_user = get_jwt_identity()
+        kunden=Kunde.query.all() 
+        return [{'kd_nr':kunde.kd_nr, 'vorname':kunde.vorname}for kunde in kunden]
+    
+
+api.add_resource(kunde_list, '/kunden')
+
+
+if __name__== '__main__':
+    app.run(debug=True)
+
---
+++ flask_api_7.py
@@ -0,0 +1,242 @@
+from flask import Flask, request, jsonify
+from typing import List
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import PrimaryKeyConstraint, func
+from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from flask_caching import Cache
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from datetime import datetime, date, timedelta
+
+app = Flask(__name__)
+api = Api(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres_user:postgres_pw@192.168.178.52:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['JWT_SECRET_KEY'] ='super_duper_secret'
+
+app.config['CACHE_TYPE'] = 'SimpleCache'
+app.config['CACHE_DEFAULT_TIMEOUT']=300     #globales Caching --> Aktualisierung des Cache alle 300 Sekunden
+
+cache = Cache(app)
+
+jwt = JWTManager(app)
+
+@app.route("/login", methods=['POST'])              ##JWT Initialisierung über /login
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if username != 'test' or password != 'test':
+        return jsonify({'msg':'Bad username or password'}), 401
+    access_token = create_access_token(identity = username)
+    return jsonify(access_token=access_token) 
+
+#Hier wird die Datenbank anhand der Model-Klassen definiert
+db = SQLAlchemy(app)
+
+#Model definieren
+class Kunde(db.Model):
+    __tablename__ = 'kunde'
+    kd_nr:Mapped[int]=mapped_column(primary_key = True)
+    vorname:Mapped[str]
+    nachname:Mapped[str]
+    strasse:Mapped[str]
+    plz:Mapped[int]
+    ort:Mapped[str]
+    vorwahl:Mapped[str]
+    telefon:Mapped[str]
+    geburtsdatum:Mapped[date]
+    ledig:Mapped[int]
+    rabatt:Mapped[float]
+    letzter_zugriff:Mapped[datetime]
+    k_a:Mapped[List["Auftrag"]]=relationship()
+
+class Auftrag(db.Model):
+    __tablename__ = 'auftrag'
+    auft_nr:Mapped[int]=mapped_column(primary_key = True)
+    bestelldat:Mapped[date]
+    lieferdat:Mapped[date]
+    zahlungsziel:Mapped[date]
+    zahlungseingang:Mapped[date]
+    mahnung:Mapped[int]
+    fk_kunde:Mapped[int]=mapped_column(ForeignKey("kunde.kd_nr"))                   #Foreign key auf kunde.kd_nr  
+    fk_shop:Mapped[int]=mapped_column(ForeignKey("shop.shop_nr"))                   #Foreign key auf shop.shop_nr
+    
+
+
+class Bestellposition(db.Model):
+    __tableame__='bestellposition'
+    fk_auftrag:Mapped[int]=mapped_column(ForeignKey("auftrag.auft_nr"))
+    position:Mapped[int]
+    fk_artikel:Mapped[int]=mapped_column(ForeignKey("artikel.art_nr"))
+    anzahl:Mapped[int]
+
+    __table_args__=(
+            PrimaryKeyConstraint('fk_auftrag', 'fk_artikel'),
+            )
+
+class Artikel(db.Model):
+    __tablename__='artikel'
+    art_nr:Mapped[int]=mapped_column(primary_key=True)
+    artikelbezeichnung:Mapped[str]
+    einzelpreis:Mapped[float]
+    gewicht:Mapped[float]
+    fk_hersteller:Mapped[float]=mapped_column(ForeignKey("hersteller.herst_nr"))
+    
+
+class Hersteller(db.Model):
+    __tablename__='hersteller'
+    herst_nr:Mapped[int]=mapped_column(primary_key=True)
+    herstellerbezeichnung:Mapped[str]
+
+class Stadt(db.Model):
+    __tablename__='stadt'
+    stadt_nr:Mapped[int]=mapped_column(primary_key=True)
+    stadt:Mapped[str]
+    lat:Mapped[float]
+    lot:Mapped[float]
+
+class Shop(db.Model):
+    __tablename__='shop'
+    shop_nr:Mapped[int]=mapped_column(primary_key=True)
+    fk_shoptyp:Mapped[int]
+    strasse:Mapped[str]
+    plz:Mapped[str]
+    fk_stadt:Mapped[int]=mapped_column(ForeignKey("stadt.stadt_nr"))
+    s_a:Mapped[List["Auftrag"]]=relationship()                                         
+
+#Initialisierung des Services
+
+class Kunde_list(Resource):
+    @cache.cached(timeout=60)      #routenspezifisches Caching --> Erneuerung des Cache alle 60 Sekunden
+#    @jwt_required()
+    def get(self):                              #Service Initialisierung   
+        now = datetime.utcnow().isoformat()
+        print(f"Uhrzeit der Generierung des Cache: {now}")
+#        current_user = get_jwt_identity()
+
+        #eigentlicher Service startet hier
+        kunden=Kunde.query.all() 
+        return [{'kd_nr':kunde.kd_nr, 'vorname':kunde.vorname}for kunde in kunden]
+    
+
+
+#service_a: 
+#Neuen Kunden anlegen. 
+#Auftrag erstellen. 
+#Bestellpositionen hinzufügen von 
+#vorhandenen Artikeln mit 
+#vorhandenen Herstellern.
+
+
+class Kunde_add(Resource):  
+    #@cache.cached(timeout=60)
+    #@jwt_required()
+    
+    def post(self):
+        data=request.get_json()     #hier wird die gesamte im URL-Body mitgeteilte JSOn entgegengenommen
+                                            #Welche Daten benötigt die Datenbank für einen vollständigen Eintrag ? --> Wie soll die JSON-Datenstruktur aussehen ? 
+        neue_kd_nr=db.session.query(func.max(Kunde.kd_nr)).scalar()+1 
+        letzter_zugriff_aktuell=datetime.utcnow().isoformat()
+      
+        kunde_neu=Kunde(
+                kd_nr=neue_kd_nr,
+                vorname=data['vorname'],
+                nachname=data['nachname'],
+                strasse=data['strasse'],
+                plz=data['plz'],
+                ort=data['ort'],
+                vorwahl=data['vorwahl'],
+                telefon=data['telefon'],
+                geburtsdatum=data['geburtsdatum'],
+                ledig=data['ledig'],
+                rabatt=data['rabatt'],
+                letzter_zugriff=letzter_zugriff_aktuell)
+        db.session.add(kunde_neu)
+        db.session.commit()
+        print(f"Kunde -{neue_kd_nr}- angelegt")
+        return {"message": "Alles erstellt."}, 201
+
+class Service_A(Resource):          #Was hier noch fehlt, ist: 
+                                    #Eine Überprüfung, ob der Käufer bereits existiert. 
+                                    #                   --> Wenn nicht, dann neu anlegen
+                                    #                   --> Wenn ja, dann artikel etc. zu einem bekannten Kunden hinzufügen
+                                    #Eine Überprüfung, ob der Artikel in der Menge auf Lager ist,  
+                                    #                   --> Wenn ja, dann Artikel aus Lager nehmen-
+                                    #                   --> Wenn nein, Bestellung stornieren
+                                    #
+    def post(self):
+
+        data=request.get_json()                 #daten aus JSON-Body auslesen
+
+        neue_kd_nr=db.session.query(func.max(Kunde.kd_nr)).scalar()+1               #neue kd_nr
+        neue_auft_nr=db.session.query(func.max(Auftrag.auft_nr)).scalar()+1         #neue auft_nr
+        aktuelle_zeit=datetime.utcnow().isoformat()                                 #aktuelle_zeit
+        aktuelles_datum=date.today()
+
+        kunde_data=data.get("kunde")
+        auftrag_data=data.get("auftrag")
+        bestellpositionen_data=data.get("bestellpositionen")
+
+        kunde_neu=Kunde(
+                kd_nr=neue_kd_nr,
+                vorname=kunde_data['vorname'],
+                nachname=kunde_data['nachname']
+                strasse=kunde_data['strasse'],
+                plz=kunde_data['plz'],
+                ort=kunde_data['ort'],
+                vorwahl=kunde_data['vorwahl'],
+                telefon=kunde_data['telefon'],
+                geburtsdatum=kunde_data['geburtsdatum'],
+                ledig=kunde_data['ledig'],
+                rabatt=kunde_data['rabatt'],
+                letzter_zugriff=aktuelle_zeit)
+        db.session.add(kunde_neu)
+
+        auftrag_neu=Auftrag(
+                auft_nr=neue_auft_nr,
+                bestelldat=aktuelles_datum,
+                lieferdat=aktuelles_datum+timedelta(days=1),
+                zahlungsziel=aktuelles_datum+timedelta(days=21),
+                zahlungseingang=aktuelles_datum,
+                mahnung=0,
+                fk_kunde=neue_kd_nr,
+                fk_shop=auftrag_data['fk_shop']                      
+                )
+        db.session.add(auftrag_neu)
+        
+        for best_position in bestellpositionen_data:
+            bestellposition_neu = Bestellposition(
+                fk_auftrag=neue_auft_nr,
+                fk_artikel=best_position['fk_artikel'],
+                position=best_position['position'],
+                anzahl=best_position['anzahl']
+                )
+            db.session.add(bestellposition_neu)
+        
+        
+
+        db.session.commit()
+        print(f"Kunde -{neue_kd_nr}- angelegt")
+        print(f"Auftrag -{neue_auft_nr}- angelegt")
+        for best_position in bestellpositionen_data:
+            print(f"Bestellposition angelegt")
+        return {"message": "Alles erstellt."}, 201
+
+   
+class Get_max_kd_nr(Resource):
+    def get(self):
+        max_kd_nr = db.session.query(db.func.max(Kunde.kd_nr)).scalar()
+        return {"kd_nr_max":max_kd_nr or 0}, 200
+
+api.add_resource(Kunde_list, '/api/kunden/list')         #gibt auskunft über alle Kunden {"kd_nr": XYZ, "vorname": "Maximilian"}
+api.add_resource(Get_max_kd_nr, '/api/kunden/get_max_kd_nr')
+api.add_resource(Kunde_add, '/api/kunden/add')
+
+api.add_resource(Service_A, '/api/service/A')
+
+if __name__== '__main__':
+    app.run(debug=True)
+
